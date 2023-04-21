6b0a69508cf2139e299d243e49fc037168e38f39
subhasis4502 <subhasis4502@gmail.com>
2023-04-21 12:53:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+let productiveActivities = [
+  {
+    label: "Reading Time",
+    id: "book",
+    rate: 100,
+  },
+  {
+    label: "Learning Coding Skill",
+    id: "coding",
+    rate: 150,
+  },
+  {
+    label: "Investment Planning",
+    id: "investment",
+    rate: 200,
+  }
+];
+
+const activitiesHTML = productiveActivities
+  .map(
+    (activity) => `
+  <div class="element">
+    <label for="${activity.id}">${activity.label}: </label>
+    <input type="number" id="${activity.id}">
+  </div>
+`
+  )
+  .join("");
+
+document.getElementById("form").innerHTML =
+  activitiesHTML +
+  `
+  <button type="submit">Submit</button>
+  <button type="reset">Reset</button>
+`;
+
+function handleSubmit(e) {
+  e.preventDefault();
+  let activityTime = 0;
+  for (let i = 0; i < productiveActivities.length; i++) {
+    const activity = productiveActivities[i];
+    let time = parseInt(document.getElementById(activity.id).value);
+    activityTime += time * activity.rate;
+  }
+  const currentScore = parseInt(document.getElementById("score").textContent);
+  const totalTime = currentScore + activityTime;
+  document.getElementById("score").textContent = totalTime;
+
+  // Store the totalTime in localStorage
+  localStorage.setItem("score", totalTime);
+}
+
+function resetScore() {
+  if (confirm("Are you sure you want to reset your score to 0?")) {
+    localStorage.setItem("score", 0);
+    location.reload();
+  }
+}
+
+
+function loadData() {
+  let score = localStorage.getItem("score");
+  if (score) {
+    document.getElementById("score").innerHTML = score;
+  } else {
+    document.getElementById("score").innerHTML = 0;
+  }
+}
+
+loadData();
